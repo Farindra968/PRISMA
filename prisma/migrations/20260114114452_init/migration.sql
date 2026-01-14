6e8cd8eb95e7f04f0fc257f75b93014a4ba27b56
farindra968 <farindrabahadurbhandari@gmail.com>
2026-01-14 17:35:02 +0545

    - migration

---
+++ prisma/migrations/20260114114452_init/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the `t` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "t";
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_phone_key" ON "user"("phone");
